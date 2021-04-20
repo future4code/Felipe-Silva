ff363be9eb5ec2ce954ccc50cad431cba141205d
FelipeAssis27 <assisfelipe.gomes@gmail.com>
2021-04-20 13:47:36 -0300

    subindo exercicios aula10 - javascript

---
+++ semana3/variaveis/exercicio3.js
@@ -0,0 +1,11 @@
+let foods = ['cenoura', 'xucrutes', 'chorumelo', 'coxinha', 'negresco'];
+console.log(foods);
+console.log("Essas são minhas comidas prediletas: ");
+console.log(foods[0]);
+console.log(foods[1]);
+console.log(foods[2]);
+console.log(foods[3]);
+console.log(foods[4]);
+
+foods[1] = prompt("Qual a sua comida favorita?");
+console.log(foods);
