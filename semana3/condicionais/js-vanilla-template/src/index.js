543e79e03cc24c524caa710d72bcefa1b7de5757
FelipeAssis27 <assisfelipe.gomes@gmail.com>
2021-04-25 15:32:32 -0300

    subindo desafios da aula 13

--- semana3/condicionais/js-vanilla-template/src/index.js
+++ semana3/condicionais/js-vanilla-template/src/index.js
@@ -199,44 +199,109 @@ par (0,0 - valor 0), (0,1 - valor 1), (1,2 - valor 3) - (3,3 - valor 6), (6,4 - 
 à variavel numero, menor que o menor valor do array, e ir incrementando e substituindo a variavel
 ate ela atingir o maior valor do array, 30. 
 
-desafio 1 - não compreendi. 
+desafio 1 - O programa recebe a quantidade de linhas pedida pelo usuario e,
+enquanto a variavel 'quantidadeAtual' for menor que o input, cria espaços vazios
+que serão printados posteriormente. No segundo loop, 'for', a variavel 'asteriscos' é 
+incrementada tendo como parâmetro de parada o valor final de 'quantidade atual' + 1, 
+acrescentando '0's aos espaços vazios das linhas. Também poderia servir de parametro 
+a expressão 'asteriscos < quantidadeTotal'.  
 */
 
-//3a) 
+//3a)
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 for (let num = 0; num < array.length; num += 1) {
-    console.log(array[num]);
-} 
+  console.log(array[num]);
+}
 //b)
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 for (let num = 0; num < array.length; num += 1) {
-    let newArray = array[num] / 10;
-    console.log(newArray);
+  let newArray = array[num] / 10;
+  console.log(newArray);
 }
 //c)
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 let parArray = [];
 for (let num = 0; num < array.length; num += 1) {
-    if ( array[num] % 2 == 0) {
-        parArray = array[num];
-    }
-    console.log(parArray);
+  if (array[num] % 2 == 0) {
+    parArray = array[num];
+  }
+  console.log(parArray);
 }
 //d)
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 for (let num = 0; num < array.length; num += 1) {
-    console.log(`O elemento do index ${num} é: ${array[num]}`);    
+  console.log(`O elemento do index ${num} é: ${array[num]}`);
 }
 //e)
 const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55];
 let maxValue = 0;
 let minValue = 1000;
 for (let num = 0; num < array.length; num += 1) {
-    if (maxValue < array[num]) {
-        maxValue = array[num];
-    }
-    if (minValue > array[num]) {
-        minValue = array[num];
-    }
+  if (maxValue < array[num]) {
+    maxValue = array[num];
+  }
+  if (minValue > array[num]) {
+    minValue = array[num];
+  }
 }
 console.log(`O maior número é ${maxValue} e o menor é ${minValue}.`);
+
+// desafios
+
+//1 -
+const key = Number(prompt('Digite o nº secreto:'));
+let count = 0;
+let x = [];
+
+alert("Vamos jogar!");
+let kicks = Number(prompt("Escolha um número de 0 a 30:"));
+alert(`O número chutado foi: ${kicks}`);
+
+while (kicks != key) {
+  if (kicks > key) {
+    alert("Errou! O nº escolhido é maior");
+    count++;
+  } else {
+    alert("Errou! O nº escolhido é menor");
+    count++;
+  }
+  x = kicks;
+  kicks = Number(prompt("Escolha um número:"));
+  alert(`O número chutado foi: ${kicks}`);
+}
+
+if (kicks == key) {
+  alert("Acertou!");
+  count++;
+}
+
+alert(`O número de tentativas foi: ${count}`);
+
+//2 -
+const key = Math.floor((Math.random() * 100) + 1);
+let count = 0;
+let x = [];
+
+alert("Vamos jogar!");
+let kicks = Number(prompt("Escolha um número de 1 a 100:"));
+alert(`O número chutado foi: ${kicks}`);
+
+while (kicks != key) {
+  if (kicks > key) {
+    alert("Errou! O nº escolhido é maior");
+    count++;
+  } else {
+    alert("Errou! O nº escolhido é menor");
+    count++;
+  }
+  x = kicks;
+  kicks = Number(prompt("Escolha um número:"));
+  alert(`O número chutado foi: ${kicks}`);
+}
+
+if (kicks == key) {
+  alert("Acertou!");
+  count++;
+}
+
+alert(`O número de tentativas foi: ${count}`);
