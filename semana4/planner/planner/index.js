e31c3dc370883563ee41a69e65f20e66c84fa12a
FelipeAssis27 <assisfelipe.gomes@gmail.com>
2021-04-27 19:12:51 -0300

    subindo atividades do dia - apenas desafio1 feito

---
+++ semana4/planner/planner/index.js
@@ -0,0 +1,44 @@
+//1))
+function criarTarefa() {
+    //2)
+    let task = document.getElementById('tarefa');
+    //3)
+    let diasDaSemana = document.getElementById('dias-semana');
+    let diaEscolhido = diasDaSemana.value;
+    
+    //4)
+    //console.log(task.value)
+    //console.log(diasDaSemana)
+    //console.log(diaEscolhido)
+    
+//5)(com desafio1)
+if (task.value !==''){
+    switch (diasDaSemana.value) {
+        case "domingo":
+            diaEscolhido.innerHTML += `<p>${task.value}</p>`;
+            break;
+        case "segunda":
+            diaEscolhido.innerHTML += `<p>${task.value}</p>`;
+            break;
+        case "terca":
+            diaEscolhido.innerHTML += `<p>${task.value}</p>`;
+            break;
+        case "quarta":
+            diaEscolhido.innerHTML += `<p>${task.value}</p>`;
+            break;
+        case "quinta":
+            diaEscolhido.innerHTML += `<p>${task.value}</p>`;
+            break;
+        case "sexta":
+            diaEscolhido.innerHTML += `<p>${task.value}</p>`;
+            break;
+        case "sabado":
+            diaEscolhido.innerHTML += `<p>${task.value}</p>`;
+            break;
+        }
+    } else {
+        alert("Por favor, insira uma tarefa!");
+    }    
+//6)
+    task = document.getElementById('');
+}
