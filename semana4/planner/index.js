7d4bc2cb315c3f9836dfc023db7d5247375bf5d0
FelipeAssis27 <assisfelipe.gomes@gmail.com>
2021-04-28 14:59:52 -0300

    atualizacao

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -16,43 +16,47 @@ if (task.value !==''){
     switch (diasDaSemana.value) {
         case "domingo":
             diaEscolhido.innerHTML += `<p>${task.value}</p>`;
-            //diaEscolhido.innerHTML += `<p onclick='scratchText()'>${task.value}</p>`;
+            diaEscolhido.innerHTML += `<p onclick='scratchText(this)'>${task.value}</p>`;
             break;
         case "segunda":
             diaEscolhido.innerHTML += `<p>${task.value}</p>`;
-            //diaEscolhido.innerHTML += `<p onclick='scratchText()'>${task.value}</p>`;
+            diaEscolhido.innerHTML += `<p onclick='scratchText(this)'>${task.value}</p>`;
             break;
         case "terca":
             diaEscolhido.innerHTML += `<p>${task.value}</p>`;
-            //diaEscolhido.innerHTML += `<p onclick='scratchText()'>${task.value}</p>`;
+            diaEscolhido.innerHTML += `<p onclick='scratchText(this)'>${task.value}</p>`;
             break;
         case "quarta":
             diaEscolhido.innerHTML += `<p>${task.value}</p>`;
-            //diaEscolhido.innerHTML += `<p onclick='scratchText()'>${task.value}</p>`;
+            diaEscolhido.innerHTML += `<p onclick='scratchText(this)'>${task.value}</p>`;
             break;
         case "quinta":
             diaEscolhido.innerHTML += `<p>${task.value}</p>`;
-            //diaEscolhido.innerHTML += `<p onclick='scratchText()'>${task.value}</p>`;
+            diaEscolhido.innerHTML += `<p onclick='scratchText(this)'>${task.value}</p>`;
             break;
         case "sexta":
             diaEscolhido.innerHTML += `<p>${task.value}</p>`;
-            //diaEscolhido.innerHTML += `<p onclick='scratchText()'>${task.value}</p>`;
+            diaEscolhido.innerHTML += `<p onclick='scratchText(this)'>${task.value}</p>`;
             break;
         case "sabado":
             diaEscolhido.innerHTML += `<p>${task.value}</p>`;
-            //diaEscolhido.innerHTML += `<p onclick='scratchText()'>${task.value}</p>`;
+            diaEscolhido.innerHTML += `<p onclick='scratchText(this)'>${task.value}</p>`;
             break;
         }
     } else {
         alert("Por favor, insira uma tarefa!");
     }    
 //6)
-    task = document.getElementById('');
+    task.value = '';
 }
 
 //desafio2
-/*function scratchText() {
-    let risk = document.getElementById('tarefa');
+function scratchText(risk) {
     risk.style = 'text-decoration: line-through';
 }
-*/
+function addTask() {
+    const meuInput = document.getElementById('tarefa')
+    const taskItems = document.getElementById('itens')
+
+    taskItems.innerHTML += `<li>${meuInput.value}</li>`
+}
